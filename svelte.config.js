0a874e5abab43e86eeffd0701da4f469dbe64ff9
Jefferson Hovey <jefferson.hovey@gmail.com>
2024-08-27 18:34:18 -0400

    style: format

--- svelte.config.js
+++ svelte.config.js
@@ -3,9 +3,9 @@ import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  paths: { base: process.env.NODE_ENV === "production" ? "/Search-Algorithm-Visualizations" : "" },
   preprocess: vitePreprocess(),
   kit: { adapter: adapter() },
-  paths: { base: process.env.NODE_ENV === "production" ? "/Search-Algorithm-Visualizations" : "" }
 };
 
 export default config;
