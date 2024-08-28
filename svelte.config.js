5c3b1b726401061a72d12cf6c3b63cfb603b1445
Jefferson Hovey <jefferson.hovey@gmail.com>
2024-08-28 11:03:43 -0400

    fix: no-undef lint error

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,7 @@ import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  /* eslint-disable-next-line no-undef */
   paths: { base: process.env.NODE_ENV === "production" ? "/Search-Algorithm-Visualizations" : "" },
   preprocess: vitePreprocess(),
   kit: { adapter: adapter() },
