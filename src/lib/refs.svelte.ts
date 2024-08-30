1bc7006ec12bf2a04e907d2f11248ef497939ecf
Jefferson Hovey <jefferson.hovey@gmail.com>
2024-08-30 15:22:34 -0400

    refactor: this works now, remove deepCopy

--- src/lib/refs.svelte.ts
+++ src/lib/refs.svelte.ts
@@ -18,12 +18,9 @@ type RefStepList = {
   goPressed: boolean;
 } & Ref<Step[]>;
 
-const deepCopy = (obj: object): object => JSON.parse(JSON.stringify(obj)) as object;
-
 const ref = <T>(initial: T): Ref<T> => {
-  const isObj = typeof initial === "object" ? true : false;
-  let v: T = $state(isObj ? (deepCopy(initial!) as T) : initial);
-  const reset = (): T => (v = isObj ? (deepCopy(initial!) as T) : initial);
+  let v: T = $state(initial);
+  const reset = (): T => (v = initial);
 
   return {
     set v(value: T) { v = value; },
