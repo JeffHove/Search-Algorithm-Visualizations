8e0e695b6bfbc61ecc57e9bef38edc4d04bdd15c
Jefferson Hovey <jefferson.hovey@gmail.com>
2024-08-31 11:57:58 -0400

    refactor: move Content to content dir

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import Footer from "$lib/components/footer/Footer.svelte";
-  import Content from "$lib/components/Content.svelte";
+  import Content from "$lib/components/content/Content.svelte";
   import Header from "$lib/components/Header.svelte";
   import "$lib/../app.css";
 </script>
