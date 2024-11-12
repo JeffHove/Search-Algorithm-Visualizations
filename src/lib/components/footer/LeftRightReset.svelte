97641fb2f811ac9f480d112b9e7d18e9892d9a0b
Jefferson Hovey <jefferson.hovey@gmail.com>
2024-11-11 19:01:35 -0500

    feat: slide animation LeftRightReset

--- src/lib/components/footer/LeftRightReset.svelte
+++ src/lib/components/footer/LeftRightReset.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { stepListIndexer, stepList, step } from "$lib/refs.svelte";
-  import { fly } from "svelte/transition";
+  import { slide } from "svelte/transition";
 
   const onclick = () => {
     step.v = $state.snapshot(stepList.v[0]);
@@ -13,7 +13,7 @@
   <button
     class="fa-solid fa-arrow-left mx-px w-12 bg-secondary-color transition active:bg-primary-color active:transition-none hover:bg-white hover:text-secondary-color"
     onclick={() => { stepListIndexer.v--; step.v = $state.snapshot(stepList.v[stepListIndexer.v]); }}
-    transition:fly={{ duration: 1000, y: 80 }}
+    transition:slide={{ duration: 1000, axis: "x" }}
     aria-label="Left arrow"
   ></button>
 {/if}
@@ -21,7 +21,7 @@
   <button
     class="fa-solid fa-arrow-right mx-px w-12 bg-secondary-color transition active:bg-primary-color active:transition-none hover:bg-white hover:text-secondary-color"
     onclick={() => { stepListIndexer.v++; step.v = $state.snapshot(stepList.v[stepListIndexer.v]); }}
-    transition:fly={{ duration: 1000, y: 80 }}
+    transition:slide={{ duration: 1000, axis: "x" }}
     aria-label="Right arrow"
   ></button>
 {/if}
